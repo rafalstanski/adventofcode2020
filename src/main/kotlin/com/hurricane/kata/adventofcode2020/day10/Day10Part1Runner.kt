f02213e743ff3e403e6623b89cf0a1a8809f367c
Rafal Stanski <>
2020-12-09 14:10:41 +0100

    Day 10 framework.

---
+++ src/main/kotlin/com/hurricane/kata/adventofcode2020/day10/Day10Part1Runner.kt
@@ -0,0 +1,13 @@
+package com.hurricane.kata.adventofcode2020.day10
+
+import com.hurricane.kata.adventofcode2020.shared.PuzzleAnswer
+import com.hurricane.kata.adventofcode2020.shared.PuzzleInput
+import com.hurricane.kata.adventofcode2020.shared.PuzzleSolution
+
+class Day10Part1Runner : PuzzleSolution {
+
+    override fun solve(puzzleInput: PuzzleInput): PuzzleAnswer {
+        return PuzzleAnswer(null)
+    }
+
+}
